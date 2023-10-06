0ad9de20180c7d3c172ca54e15ef5b5b9764348e
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-10-06 02:02:54 -0700

    Conexión actualizada

--- ProyectoMakeItReal/utils/connection.js
+++ ProyectoMakeItReal/utils/connection.js
@@ -1,57 +1,9 @@
+const { Sequelize } = require('sequelize');
 
-const { Sequelize, DataTypes } = require('sequelize');
-
-
-class Conexion{
-    constructor(configuracion){
-        this.configuracion=configuracion;
-        this.sequelize=new Sequelize(configuracion)
-    }
-
-   async conectar(){
-    try {
-       await this.sequelize.authenticate();
-        console.log("Conectado");
-    } catch (error) {
-        throw new Error("Error al conectar");
-    }   
-    }
-
-  async desconectar(){
-
-        try {
-            await this.sequelize.close();
-            console.log("Desconectado");
-        } catch (error) {
-            throw new Error("Error al desconectar");
-        }
-    
-
-    }
-}
-
-
-//PRUEBA
-const configuracionDB = {
-    dialect: 'mysql',
+const sequelize = new Sequelize('makeitreal', 'root', '2206', {
     host: 'localhost',
-    username: 'root',
-    password: '123456',
-    database: 'makeitreal',
-};
+    dialect: 'mysql'
+});
 
-async function probarConexion() {
-    const conexion = new Conexion(configuracionDB);
-
-    try {
-        await conexion.conectar();
-        
-   
-        await conexion.desconectar();
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-probarConexion();
+module.exports = sequelize;
 
