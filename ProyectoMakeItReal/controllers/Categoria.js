57772aa3beb470e4a8d65c463127a4ac30f03be9
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-10-25 17:51:59 -0700

    Rest + BD de Categoría

--- ProyectoMakeItReal/controllers/Categoria.js
+++ ProyectoMakeItReal/controllers/Categoria.js
@@ -2,13 +2,12 @@ const Categoria = require('../modelos/Categoria.js');
 
 exports.addCategoria = async (req, res) => {
     try {
-        //const nuevaCategoria = await Categoria.create(req.body);
         const { nombre, descripcion } = req.body;
-        const nuevaCategoria = {
-            id: 1,
+        
+        const nuevaCategoria = await Categoria.create({
             nombre,
             descripcion
-        };
+        });
 
         res.status(201).json({
             status: 'success',
@@ -19,88 +18,40 @@ exports.addCategoria = async (req, res) => {
     } catch (error) {
         res.status(400).json({
             status: 'fail',
-            message: 'Error al agregar categoría'+error
+            message: 'Error al agregar categoría: ' + error.message
         });
     }
 };
 
 exports.getCategorias = async (req, res) => {
     try {
-        
-        //const categorias = await Categoria.findAll();
-        
-        const categorias = [
-            {
-                id: 1,
-                nombre: "Categoria consultada 1(consulta de todas)",
-                descripcion: "Descripción de la categoría 1"
-            },
-            {
-                id: 2,
-                nombre: "Categoria consultada 2",
-                descripcion: "Descripción de la categoría 2"
-            }
-        ];
-        
+        const categorias = await Categoria.findAll();
+
         res.status(200).json({
             status: 'success',
             data: {
-                categorias: categorias
+                categorias
             }
         });
     } catch (error) {
         res.status(400).json({
             status: 'fail',
-            message: 'Error al obtener categorías'
+            message: 'Error al obtener categorías: ' + error.message
         });
     }
 };
 
 exports.getCategoriaById = async (req, res) => {
     try {
-       // const categoria = await Categoria.findByPk(req.params.id);
-       const { id } = req.params;
-       const categoria = {
-        id: id,
-        nombre: `Nombre de la categoría buscada por el id: ${id}`,
-        descripcion: "Descripción de la categoría"
-    };
-        if (categoria) {
-            res.status(200).json({
-                status: 'success',
-                data: {
-                    categoria: categoria
-                }
-            });
-        } else {
-            res.send("Error")
-        }
-    } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            message: 'Error al obtener categoría por ID'
-        });
-    }
-};
-
-exports.updateCategoriaById = async (req, res) => {
-    try {
-        //const categoria = await Categoria.findByPk(req.params.id);
         const { id } = req.params;
-        const categoria = {
-            id: id,
-            nombre: `Nombre de la categoría actualizada por el id: ${id}`,
-            descripcion: "Descripción de la categoría"
-        };
+
+        const categoria = await Categoria.findByPk(id);
+
         if (categoria) {
-           /* await categoria.update({
-                nombre: req.body.nombre,
-                descripcion: req.body.descripcion
-            });*/
             res.status(200).json({
                 status: 'success',
                 data: {
-                    categoria: categoria
+                    categoria
                 }
             });
         } else {
@@ -112,23 +63,54 @@ exports.updateCategoriaById = async (req, res) => {
     } catch (error) {
         res.status(400).json({
             status: 'fail',
-            message: 'Error al actualizar categoría por ID'
+            message: 'Error al obtener categoría por ID: ' + error.message
+        });
+    }
+};
+
+exports.updateCategoriaById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const categoria = await Categoria.findByPk(id);
+
+        if (categoria) {
+
+            await categoria.update({
+                nombre: req.body.nombre,
+                descripcion: req.body.descripcion
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    categoria
+                }
+            });
+        } else {
+            res.status(404).json({
+                status: 'fail',
+                message: 'Categoría no encontrada'
+            });
+        }
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Error al actualizar categoría por ID: ' + error.message
         });
     }
 };
 
 exports.deleteCategoriaById = async (req, res) => {
     try {
-       // const categoria = await Categoria.findByPk(req.params.id);
+        const { id } = req.params;
+        
+        const categoria = await Categoria.findByPk(id);
 
-       const { id } = req.params;
-       const categoria = {
-        id: 1,
-        nombre: "Nombre de la categoría actualizada por id",
-        descripcion: "Descripción de la categoría"
-    };
         if (categoria) {
-           // await categoria.destroy();
+
+            await categoria.destroy();
+
             res.status(200).json({
                 status: 'success',
                 message: `Categoría ${id} eliminada correctamente`
@@ -142,7 +124,7 @@ exports.deleteCategoriaById = async (req, res) => {
     } catch (error) {
         res.status(400).json({
             status: 'fail',
-            message: 'Error al eliminar categoría por ID'
+            message: 'Error al eliminar categoría por ID: ' + error.message
         });
     }
 };
