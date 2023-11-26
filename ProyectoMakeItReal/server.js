e326be7c1bcbcf3bcfec4a0c4941c0449b81d697
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-11-26 13:41:11 -0700

    Frontend pagos y categorías (completo)

--- ProyectoMakeItReal/server.js
+++ ProyectoMakeItReal/server.js
@@ -4,6 +4,8 @@ const fileUpload = require('express-fileupload');
 const routerProyecto = require('./routes/Proyecto');
 const routerImagen = require('./routes/ImagenProyecto');
 const routerEmprendedor = require('./routes/Emprendedor');
+const routerPago = require('./routes/pago');
+const routerCategoria = require('./routes/categoria');
 const cors = require('cors');
 const PORT = 3000;
 
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/proyectos', routerProyecto);
 app.use('/imagen',routerImagen);
+app.use('/pago', routerPago);
+app.use('/categoria', routerCategoria);
 app.use( fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/',
